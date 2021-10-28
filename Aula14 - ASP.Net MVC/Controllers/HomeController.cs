e8f8ebd22c77b3e1757e15d427e9258569521ab3
nubiapeixer <nubiapeixer@hotmail.com>
2021-10-28 11:44:38 -0300

    Asp.Net mvc

---
+++ Aula14 - ASP.Net MVC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Aula_ASP.NET_core.Models;
+using BAL;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula_ASP.NET_core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult CadastroVaga()
+        {
+            return View();
+        }
+        public IActionResult AuthVaga(string placa, string horaEntrada)
+        {
+            Estacionamento vaga = new Estacionamento
+            {
+                Placa = placa,
+                HoraEntrada = Convert.ToDouble(horaEntrada)
+            };
+            Response resp = EstacionamentoBAL.Insert(vaga);
+            if (resp.Executed)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("CadastroVaga", "Home");
+            }
+        }
+    }
+}
---
+++ Aula14 - ASP.Net MVC/DAL/Estacionamento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DAL
+{
+    public class Estacionamento
+    {
+        public int idEstacionamento { get; set; }
+        public string Placa { get; set; }
+        public double HoraEntrada { get; set; }
+    }
+}
