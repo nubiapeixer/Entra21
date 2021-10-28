d2ede71a6a6176d002b90050bd3615d95a3cc163
nubiapeixer <nubiapeixer@hotmail.com>
2021-10-27 22:13:15 -0300

    ASP.Net

---
+++ Aula14 - ASP.Net/BAL/EstacionamentoBAL.cs
@@ -0,0 +1,42 @@
+﻿using DAL;
+using System;
+
+namespace BAL
+{
+    public static class EstacionamentoBAL
+    {
+        public static Response Insert(Estacionamento vaga)
+        {
+            if (!string.IsNullOrEmpty(vaga.Placa) && vaga.Placa.Length < 8)
+            {
+                return EstacionamentoDB.Insert(vaga);
+            }
+            else
+            {
+                return new Response
+                {
+                    Executed = false,
+                    ErrorMessage = "Placa vazia ou grande"
+                };
+            }
+        }
+
+        public static Response Select(int id, out Estacionamento vaga)
+        {
+            vaga = new Estacionamento();
+            if (id > 0)
+            {
+                return EstacionamentoDB.Select(id, out vaga);
+            }
+            else
+            {
+                return new Response
+                {
+                    Executed = false,
+                    ErrorMessage = "id inválido"
+                };
+            }
+        }
+
+    }
+}
